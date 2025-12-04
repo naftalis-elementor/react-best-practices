06d607a95a0120bb64c849af21093aa5138092fd
Naftal S <naftalis@elementor.com>
2025-12-04 10:44:49 +0200

    Update base URL in Vite config and adjust asset links in index.html for new project structure; remove unused JavaScript and CSS files.

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig({
     },
   },
   assetsInclude: ["**/*.jsx"],
-  base: "https://naftalis-elementor.github.io/",
+  base: "https://naftalis-elementor.github.io/react-best-practices/",
 });
